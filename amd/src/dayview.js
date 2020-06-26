032b07b1f79d6bbcab7ac0074af3c121135eb37b
Matt Porritt <mattp@catalyst-au.net>
2020-06-26 00:19:01 +0000

    daily schedule with time display

--- amd/src/dayview.js
+++ amd/src/dayview.js
@@ -21,8 +21,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-define(['core/str', 'core/notification', 'core/modal_factory', 'local_assessfreq/modal_large', 'core/templates', 'core/ajax'],
-function(Str, Notification, ModalFactory, ModalLarge, Templates, Ajax) {
+define(['core/str', 'core/notification', 'core/modal_factory', 'local_assessfreq/modal_large', 'core/templates', 'core/ajax',
+    'core/user_date'],
+function(Str, Notification, ModalFactory, ModalLarge, Templates, Ajax, UserDate) {
 
     /**
      * Module level variables.
@@ -33,42 +34,63 @@ function(Str, Notification, ModalFactory, ModalLarge, Templates, Ajax) {
         + '<i class="fa fa-spinner fa-pulse fa-2x fa-fw"></i>'
         + '</p>';
 
-    const formatData = function(response) {
+    const getUserDate = function (timestamp, format) {
         return new Promise((resolve) => {
-            let responseArr = JSON.parse(response);
+            Str.get_string(format, 'langconfig')
+            .then((dateformat) => {
+                let request = [
+                    {timestamp: timestamp,  format: dateformat}
+                ];
+                UserDate.get(request)
+                .then((userdate) => {
+                    resolve(userdate);
+                });
+            }).catch(() => {
+                Notification.exception(new Error('Failed to load string: date format'));
+            });
+        });
+    };
 
-            // We are displaying the event as a bar whose width represents the start and end time of the event.
-            // We need to scale the width of the bar to match the width of the container. Therefore 100% width of the container
-            // equals 24 hours (one day).
-            // There are 1440 mins per day. 1440 mins equals 100%, therefore 1 min = (100/1440)%. 5/72 == 100/1440.
-            let scaler = 5/72;
+    const formatData = async function(response) {
+        let responseArr = JSON.parse(response);
 
-            for (let i=0; i<responseArr.length; i++) {
-                const year = responseArr[i].endyear;
-                const month = (responseArr[i].endmonth) - 1; // Minus 1 for difference between months in PHP and JS.
-                const day = responseArr[i].endday;
-                const dayStart = (new Date(year, month, day).getTime()) / 1000;
-                let secondsSinceDayStart = responseArr[i].timestart - dayStart;
-                let leftMargin = 0;
-                let width = 0;
+        // We are displaying the event as a bar whose width represents the start and end time of the event.
+        // We need to scale the width of the bar to match the width of the container. Therefore 100% width of the container
+        // equals 24 hours (one day).
+        // There are 1440 mins per day. 1440 mins equals 100%, therefore 1 min = (100/1440)%. 5/72 == 100/1440.
+        let scaler = 5/72;
 
-                if (secondsSinceDayStart <= 0) {
-                    secondsSinceDayStart = 0;
-                    width = ((responseArr[i].timeend - dayStart) / 60) * scaler;
-                } else {
-                    leftMargin = (secondsSinceDayStart / 60) * scaler;
-                    width = ((responseArr[i].timeend - responseArr[i].timestart) / 60) * scaler;
-                }
+        for (let i=0; i<responseArr.length; i++) {
+            const year = responseArr[i].endyear;
+            const month = (responseArr[i].endmonth) - 1; // Minus 1 for difference between months in PHP and JS.
+            const day = responseArr[i].endday;
+            const dayStart = (new Date(year, month, day).getTime()) / 1000;
+            let secondsSinceDayStart = responseArr[i].timestart - dayStart;
+            let leftMargin = 0;
+            let width = 0;
 
-                if (leftMargin + width > 100) {
-                    width = 100 - leftMargin;
-                }
-
-                responseArr[i].leftmargin = leftMargin;
-                responseArr[i].width = width;
-                window.console.log(responseArr[i]);
+            if (secondsSinceDayStart <= 0) {
+                secondsSinceDayStart = 0;
+                width = ((responseArr[i].timeend - dayStart) / 60) * scaler;
+                responseArr[i].start = await getUserDate(responseArr[i].timestart, 'strftimedatetime');
+            } else {
+                leftMargin = (secondsSinceDayStart / 60) * scaler;
+                width = ((responseArr[i].timeend - responseArr[i].timestart) / 60) * scaler;
+                responseArr[i].start = await getUserDate(responseArr[i].timestart, 'strftimetime');
             }
 
+            if (leftMargin + width > 100) {
+                width = 100 - leftMargin;
+            }
+
+            responseArr[i].leftmargin = leftMargin;
+            responseArr[i].width = width;
+            responseArr[i].end = await getUserDate(responseArr[i].timeend, 'strftimetime');
+
+            window.console.log(responseArr[i]);
+        }
+
+        return new Promise((resolve) => {
             resolve(responseArr);
         });
     };
@@ -114,7 +136,7 @@ function(Str, Notification, ModalFactory, ModalLarge, Templates, Ajax) {
      */
     Dayview.init = function() {
 
-        Str.get_string('loading', 'core').then((title) => {
+        Str.get_string('schedule', 'local_assessfreq').then((title) => {
             // Create the Modal.
             ModalFactory.create({
                 type: ModalLarge.TYPE,
