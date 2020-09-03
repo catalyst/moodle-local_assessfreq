50fbaa670e8323facd0a40faad572e703740d571
Matt Porritt <mattp@catalyst-au.net>
2020-09-03 05:56:32 +0000

    update observer filter for 3.5

--- amd/src/form_modal.js
+++ amd/src/form_modal.js
@@ -42,8 +42,8 @@ function(Str, ModalFactory, Fragment, Ajax) {
     const ObserverCallback = function(mutationsList) {
         for (let i=0; i<mutationsList.length; i++) {
             let element = mutationsList[i].target;
-            window.console.log(element);
-            if(element.tagName.toLowerCase() === 'span' && element.classList.contains('tag')) {
+            if((element.tagName.toLowerCase() === 'li') && (element.getAttribute('role') == 'option')
+                    && (element.getAttribute('aria-selected') == 'true')) {
                 element.addEventListener('click', updateModalBody);
                 document.getElementById('id_courses').dataset.course = element.dataset.value;
 
