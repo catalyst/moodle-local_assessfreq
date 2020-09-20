0aabb1fb21e554150a6ec58b96ed8adaddeb028f
Matt Porritt <mattp@catalyst-au.net>
2020-09-20 01:41:57 +0000

    fix scale regeneration when metric changs

--- amd/src/dashboard_assessment.js
+++ amd/src/dashboard_assessment.js
@@ -169,11 +169,11 @@ function(Ajax, Fragment, Templates, Notification, Calendar, Str, ZoomModal, Dayv
 
         spinner.classList.remove('hide'); // Show spinner if not already shown.
 
-        Calendar.generate(year, 0, 11, metric, modules).then(calendar => {
+        Calendar.generate(year, 0, 11, metric, modules)
+        .then(calendar => {
             let calendarContainer = document.getElementById('local-assessfreq-report-heatmap-months');
             calendarContainer.innerHTML = calendar.innerHTML;
             calendarContainer.addEventListener('click', detailView);
-            return;
         })
         .then(Calendar.createHeatScale)
         .then((heatScale) => {
