b2a842c1f3050b200e9ba0b7c4e565232a8a6f76
Matt Porritt <mattp@catalyst-au.net>
2020-10-28 15:10:25 +1100

    extend quiz class to include course and override links

--- tests/quiz_test.php
+++ tests/quiz_test.php
@@ -134,7 +134,7 @@ class quiz_testcase extends advanced_testcase {
     /**
      * Set up conditions for tests.
      */
-    public function setUp() {
+    public function setUp(): void {
         $this->resetAfterTest();
 
         global $DB, $CFG;
