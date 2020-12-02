79e946e091f1d5571310dab969b245e4ca045cbb
Matt Porritt <mattp@catalyst-au.net>
2020-12-02 00:50:10 +0000

    add chart legend option support for Moodles pre 3.9

--- amd/src/chart_output_chartjs.js
+++ amd/src/chart_output_chartjs.js
@@ -27,6 +27,7 @@ define(['core/chart_output_chartjs'], function(Output) {
      */
     var ChartOutput = {};
     var aspectRatio = false;
+    var rtLegendoptions = false;
 
     /**
      * Overrride the config.
@@ -52,6 +53,11 @@ define(['core/chart_output_chartjs'], function(Output) {
         var legendOptions = this._chart.getLegendOptions();
         if (legendOptions) {
             config.options.legend = legendOptions;
+        }
+
+        // Override legend options with those provided at run time.
+        if (rtLegendoptions)  {
+            config.options.legend = rtLegendoptions;
         }
 
         this._chart.getXAxes().forEach(function(axis, i) {
@@ -98,8 +104,9 @@ define(['core/chart_output_chartjs'], function(Output) {
     /**
      * Get the aspect ratio setting and initialise the chart.
      */
-    ChartOutput.init = function(chartImage, ChartInst, aspect) {
+    ChartOutput.init = function(chartImage, ChartInst, aspect, legend) {
         aspectRatio = aspect;
+        rtLegendoptions = legend;
         new Output(chartImage, ChartInst);
     };
 
